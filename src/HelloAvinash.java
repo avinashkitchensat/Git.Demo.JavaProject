196c549d1ce6d597c1d6ca00731fcb901c6ff86f
avinashkitchensat <developer@loyalhospitality.in>
2019-08-01 13:32:35 +0530

    Seventh Commit

--- src/HelloAvinash.java
+++ src/HelloAvinash.java
@@ -3,13 +3,19 @@ public class HelloAvinash {
 
 	public static void main(String[] args) {
 
-		//System.out.println("Hello Avinash..!");
+		System.out.println("Hello Avinash..!");
 		System.out.println("Hello Avinash You Are Great..!");
 		System.out.println("Hello Avinash You Are Awesome..!");
 		System.out.println("Hello Avinash You Are Impressive..!");
 		System.out.println("Hello Avinash Great Going..!");
 		System.out.println("Hello Avinash Success is near to You..!");
 
+		System.out.println("Hello Avinash..!");
+		System.out.println("Hello Avinash You Are Great..!");
+		System.out.println("Hello Avinash You Are Awesome..!");
+		System.out.println("Hello Avinash You Are Impressive..!");
+		System.out.println("Hello Avinash Great Going..!");
+		System.out.println("Hello Avinash Success is near to You..!");
 	}
 
 }
